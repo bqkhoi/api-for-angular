c4c4719de0f3fb2a0598d87bb2519f5669a71825
Khoi Bui <bqkhoi29@gmail.com>
2024-09-29 08:21:19 +0700

    remove irrelevant code and optimize delete API

--- src/main/java/khoi/bui/api_for_angular/controller/PlaceController.java
+++ src/main/java/khoi/bui/api_for_angular/controller/PlaceController.java
@@ -1,35 +1,30 @@
 package khoi.bui.api_for_angular.controller;
 
 import khoi.bui.api_for_angular.dto.PlacesDto;
-import khoi.bui.api_for_angular.entity.Place;
-import khoi.bui.api_for_angular.mapper.PlaceMapper;
 import khoi.bui.api_for_angular.service.PlaceService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Comparator;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/places")
 public class PlaceController {
     private final PlaceService placeService;
-    private final PlaceMapper placeMapper;
-    public PlaceController(PlaceService placeService, PlaceMapper placeMapper){
-        this.placeService=placeService;
-        this.placeMapper = placeMapper;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
     }
 
     @GetMapping("/init")
-    public void initData(){
-         this.placeService.initData();
+    public void initData() {
+        this.placeService.initData();
     }
 
-    //@ResponseBody
-    //@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @GetMapping("")
-    public PlacesDto findAll(){
+    public PlacesDto findAll() {
         return placeService.findAll();
     }
 }
--- src/main/java/khoi/bui/api_for_angular/controller/UserPlaceController.java
+++ src/main/java/khoi/bui/api_for_angular/controller/UserPlaceController.java
@@ -1,11 +1,9 @@
 package khoi.bui.api_for_angular.controller;
 
-import jakarta.websocket.server.PathParam;
 import khoi.bui.api_for_angular.dto.PlaceDto;
 import khoi.bui.api_for_angular.dto.PlacesDto;
 import khoi.bui.api_for_angular.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,6 +14,8 @@ public class UserPlaceController {
     PlaceService placeService;
 
     @PutMapping("")
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public PlaceDto addUserPlace(@RequestBody String selectedPlaceId){
         return placeService.addUserPlace(selectedPlaceId);
     }
@@ -25,7 +25,7 @@ public class UserPlaceController {
         return placeService.findUserPlaces();
     }
 
-    @PutMapping("/{placeId}")
+    @DeleteMapping("/{placeId}")
 //    @ResponseBody
 //    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public PlaceDto removeUserPlace(@PathVariable String placeId){
